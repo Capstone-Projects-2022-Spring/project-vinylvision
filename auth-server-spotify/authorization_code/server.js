95bdbabf8dcc0bcaff58d8b0230b7620862a8331
Robert Morsa <tuj79353@temple.edu>
2022-03-12 02:09:50 -0500

    split app.js into server.js and spotify.js, made server port and localhost share the same port, added functions for getting JSON structs in spotify.js, moved app.js and old index.js into css folder (these are unused and exist only for reference), changed button on home page into a link which prompts a log-in, moved home page into public folder to run, open the command prompt and type "npm install" to install node.js, then run "node auth-server-spotify/authorization_code/server.js". this will host server.js on http://localhost:8888. open localhost:8888 and press the login button. it should show the access token in the URL

---
+++ auth-server-spotify/authorization_code/server.js
@@ -0,0 +1,115 @@
+﻿var express = require('express'); // Express web server framework
+var request = require('request'); // "Request" library
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+var serverPORT = 8888
+
+var projectUrl = 'http://localhost:8888';
+const spotify = require('./spotify.js');
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+var stateKey = 'spotify_auth_state';
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'))
+    .use(cors())
+    .use(cookieParser());
+
+//send to spotify's site to authorize user with options to return to vinylvision site
+app.get('/login', function (req, res) {
+
+    var state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    // your application requests authorization
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        spotify.getAuthQueryString(state));
+});
+
+//callback redirection function
+app.get('/callback', function (req, res) {
+
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) { //state mismatch error
+        res.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+            })); //put in url state_mismatch
+    } else {
+        res.clearCookie(stateKey);
+        var authOptions = spotify.getAuthOptions(code);
+
+        request.post(authOptions, function (error, response, body) { //access token request
+            if (!error && response.statusCode === 200) {
+
+                var access_token = body.access_token,
+                    refresh_token = body.refresh_token;
+
+                var options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+
+                // use the access token to access the Spotify Web API
+                request.get(options, function (error, response, body) {
+                    console.log(body);
+                });
+
+                // we can also pass the token to the browser to make requests from there
+                res.redirect(projectUrl + '/#' +
+                    querystring.stringify({
+                        access_token: access_token,
+                        refresh_token: refresh_token
+                    }));
+            } else { //response failure
+                res.redirect('/#' +
+                    querystring.stringify({
+                        error: 'invalid_token'
+                    })); //put invalid_token in url
+            }
+        });
+    }
+});
+
+//refresh access token
+app.get('/refresh_token', function (req, res) {
+
+    // requesting access token from refresh token
+    var refresh_token = req.query.refresh_token;
+    var authOptions = spotify.getAuthOptionsRefresh(refresh_token)
+
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) { //response success
+            var access_token = body.access_token;
+            res.send({
+                'access_token': access_token
+            });
+        }
+    });
+});
+
+console.log('Listening on ' + serverPORT);
+app.listen(serverPORT);
---
+++ auth-server-spotify/authorization_code/spotify.js
@@ -0,0 +1,50 @@
+﻿var querystring = require('querystring');
+
+var client_id = 'd0525be4c5f740699430e39162f29ca4'; // Your client id
+var client_secret = '846a8c90ce324b1cae5596c150585d64'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+var scope = 'user-read-private user-read-email'; //permissions
+
+function getAuthOptions(code) { //access token authorization options
+    return {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+            code: code,
+            redirect_uri: redirect_uri,
+            grant_type: 'authorization_code'
+        },
+        headers: { //HTTP headers
+            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        },
+        json: true
+    }
+}
+
+function getAuthOptionsRefresh(refresh_token) { //refresh access token options
+    return {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+        form: {
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        },
+        json: true
+    }
+}
+
+function getAuthQueryString(state) { //login & redirection options
+    return querystring.stringify({
+        response_type: 'code',
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+    })
+}
+
+module.exports = { //for external use of functions
+    getAuthOptions: getAuthOptions,
+    getAuthOptionsRefresh: getAuthOptionsRefresh,
+    getAuthQueryString: getAuthQueryString
+}
