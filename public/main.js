1894fe8a757fbf4d633f6a51c13d3dd365801831
tuj58508 <tuj58508@temple.edu>
2022-03-13 21:23:18 -0400

    Edits to displaying Google output

--- public/main.js
+++ public/main.js
@@ -75,8 +75,8 @@ function sendFileToCloudVision (content) {
  * Displays the results.
  */
 function displayJSON (data) {
-  var data2 = data.responses[0].webDetection.bestGuessLabels[0].label;
-  console.log(data2)
+  var data2 = ('Your album cover is: \t' + data.responses[0].webDetection.bestGuessLabels[0].label);
+  console.log(data2);
   var contents = JSON.stringify(data, null, 5);
   $('#results').text(data2);
   var evt = new Event('results-displayed');
