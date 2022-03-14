dc6037847b678fe1a41af5cc4128b8ac91281dc4
AndrewPari <77748463+AndrewPari@users.noreply.github.com>
2022-03-13 23:43:13 -0400

    initial testing of audd.io api

---
+++ test.py
@@ -0,0 +1,26 @@
+import requests
+
+
+data = {
+    'api_token': '7c1042cd5831c7c617a466cdf3f6c1a4',
+    'url': 'https://audd.tech/example.mp3',
+    'return': 'spotify',
+}
+result = requests.post('https://api.audd.io/', data=data)
+print(result.text[30:100])
+
+
+
+data = {
+    'api_token': '7c1042cd5831c7c617a466cdf3f6c1a4',
+    'return': 'apple_music,spotify',
+}
+files = {
+    'file': open('c:/Users/Tepex/Desktop/roundabout.mp3', 'rb')
+}
+result = requests.post('https://api.audd.io/', data=data, files=files)
+print(result.text[30:100])
+
+
+
+
