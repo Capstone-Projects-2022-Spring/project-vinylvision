9e10350edfafc30374e17ec68554b31748637417
AndrewPari <77748463+AndrewPari@users.noreply.github.com>
2022-03-25 19:07:54 -0400

    microphone recording + download skeleton

--- testing/testing/app.js
+++ testing/testing/app.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+var data = {
+    'api_token': '7c1042cd5831c7c617a466cdf3f6c1a4',
+    'url': 'https://audd.tech/example.mp3',
+    'return': 'apple_music,spotify',
+};
+
+$.getJSON("https://api.audd.io/?jsonp=?", data, function (result) {
+    document.write(result);
+});
