a685352ce8e9ef2ac5beb419f1e21e5995e99fb7
Robert Morsa <tuj79353@temple.edu>
2022-03-27 02:51:22 -0400

    created integration between google vision and spotify player using cookies (i wish i could think of an easier way), this cookie is labellled "guess" and if this is found it will automatically search, adds the spotify search button after the google vision guess result appears. this cookie will reset once it has been received, changed the login to spotify button to the search with spotify button

---
+++ public/main.js
@@ -0,0 +1,91 @@
+﻿// Copyright 2015, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+var CV_URL = 'https://vision.googleapis.com/v1/images:annotate?key=' + window.apiKey;
+
+$(function () {
+    $('#fileform').on('submit', uploadFiles);
+});
+
+/**
+ * 'submit' event handler - reads the image bytes and sends it to the Cloud
+ * Vision API.
+ */
+function uploadFiles(event) {
+    event.preventDefault(); // Prevent the default form post
+
+    // Grab the file and asynchronously convert to base64.
+    var file = $('#fileform [name=fileField]')[0].files[0];
+    var reader = new FileReader();
+    reader.onloadend = processFile;
+    reader.readAsDataURL(file);
+}
+
+/**
+ * Event handler for a file's data url - extract the image data and pass it off.
+ */
+function processFile(event) {
+    var content = event.target.result;
+    sendFileToCloudVision(content.replace('data:image/jpeg;base64,', ''));
+}
+
+/**
+ * Sends the given file contents to the Cloud Vision API and outputs the
+ * results.
+ */
+function sendFileToCloudVision(content) {
+    var type = $('#fileform [name=type]').val();
+
+    // Strip out the file prefix when you convert to json.
+    var request = {
+        requests: [{
+            image: {
+                content: content
+            },
+            features: [{
+                type: type,
+                maxResults: 1
+            }]
+        }]
+    };
+
+    document.getElementById('login').innerHTML = null
+    $('#results').text('Loading...');
+    $.post({
+        url: CV_URL,
+        data: JSON.stringify(request),
+        contentType: 'application/json'
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        $('#results').text('ERRORS: ' + textStatus + ' ' + errorThrown);
+    }).done(displayJSON);
+}
+
+/**
+ * Displays the results.
+ */
+function displayJSON(data) {
+    if (!data) {
+
+    }
+    var data2 = ('Your album cover is: \t' + data.responses[0].webDetection.bestGuessLabels[0].label);
+    console.log(data2);
+    var contents = JSON.stringify(data, null, 5);
+    $('#results').text(data2);
+    var evt = new Event('results-displayed');
+    evt.results = contents;
+    document.dispatchEvent(evt);
+    document.cookie = "guess=" + data.responses[0].webDetection.bestGuessLabels[0].label
+    document.getElementById('login').innerHTML = "<a href='spotify/login' type='button'>Search with Spotify</a>"
+}
