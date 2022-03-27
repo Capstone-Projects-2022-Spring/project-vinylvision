dee565265e2231cdabfe93080c67b1d2d5755bbc
AndrewPari <77748463+AndrewPari@users.noreply.github.com>
2022-03-26 20:32:22 -0400

    deleting copy of directory

--- testing/testing/app.js
+++
@@ -1,9 +0,0 @@
-var data = {
-    'api_token': '7c1042cd5831c7c617a466cdf3f6c1a4',
-    'url': 'https://audd.tech/example.mp3',
-    'return': 'apple_music,spotify',
-};
-
-$.getJSON("https://api.audd.io/?jsonp=?", data, function (result) {
-    document.write(result);
-});
